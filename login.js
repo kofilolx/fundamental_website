abc14ddc54af9e567ca95e86bf569a0f5fb16a69
kofilolx <32402172+kofilolx@users.noreply.github.com>
2022-04-28 21:41:07 +0000

    Add files via upload

---
+++ login.js
@@ -0,0 +1,7 @@
+function openForm() {
+    document.getElementById("myLogin").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myLogin").style.display = "none";
+  }
